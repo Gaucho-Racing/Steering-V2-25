1137a5e786b7fea98c33222f7b0040862d6ff5a0
Casey Zwicker <caseypzwicker@gmail.com>
2025-02-20 22:35:05 -0800

    ok this time ACTUALLY cmake

--- Core/Src/stm32u5xx_it.c
+++ Core/Src/stm32u5xx_it.c
@@ -56,7 +56,6 @@
 
 /* External variables --------------------------------------------------------*/
 extern DMA2D_HandleTypeDef hdma2d;
-extern FDCAN_HandleTypeDef hfdcan1;
 extern GPU2D_HandleTypeDef hgpu2d;
 extern LTDC_HandleTypeDef hltdc;
 extern TIM_HandleTypeDef htim2;
@@ -175,20 +174,6 @@ void EXTI6_IRQHandler(void)
   /* USER CODE BEGIN EXTI6_IRQn 1 */
 
   /* USER CODE END EXTI6_IRQn 1 */
-}
-
-/**
-  * @brief This function handles FDCAN1 interrupt 0.
-  */
-void FDCAN1_IT0_IRQHandler(void)
-{
-  /* USER CODE BEGIN FDCAN1_IT0_IRQn 0 */
-
-  /* USER CODE END FDCAN1_IT0_IRQn 0 */
-  HAL_FDCAN_IRQHandler(&hfdcan1);
-  /* USER CODE BEGIN FDCAN1_IT0_IRQn 1 */
-
-  /* USER CODE END FDCAN1_IT0_IRQn 1 */
 }
 
 /**
