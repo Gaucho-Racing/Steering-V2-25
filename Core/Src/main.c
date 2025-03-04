b8cd50036f64fe6aa26933b6cc33ef559354133b
Bailey Say <baileyasay@gmail.com>
2025-03-03 21:34:05 -0800

    exmaple

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -177,14 +177,26 @@ int main(void)
   /* lvgl demo */
   lv_demo_widgets();
 
-  /*Change the active screen's background color*/
-  lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0x003a57), LV_PART_MAIN);
+  static lv_style_t style;
+  lv_style_init(&style);
+  lv_style_set_radius(&style, 5);
 
-  /*Create a white label, set its text and align it to the center*/
-  lv_obj_t * label = lv_label_create(lv_screen_active());
-  lv_label_set_text(label, "Hello world");
-  lv_obj_set_style_text_color(lv_screen_active(), lv_color_hex(0xffffff), LV_PART_MAIN);
-  lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+  /*Make a gradient*/
+  lv_style_set_width(&style, 150);
+  lv_style_set_height(&style, LV_SIZE_CONTENT);
+
+  lv_style_set_pad_ver(&style, 20);
+  lv_style_set_pad_left(&style, 5);
+
+  lv_style_set_x(&style, lv_pct(50));
+  lv_style_set_y(&style, 80);
+
+  /*Create an object with the new style*/
+  lv_obj_t * obj = lv_obj_create(lv_scr_act());
+  lv_obj_add_style(obj, &style, 0);
+
+  lv_obj_t * label = lv_label_create(obj);
+  lv_label_set_text(label, "Hello");
 
   // lv_obj_t * scr = lv_obj_create(NULL);
   // // lv_screen_load(scr);
