6fb0d7402797b11d4ad4192ced59b82d6683c334
Daniel Hansen <dchansen06@gmail.com>
2025-03-10 23:31:54 -0700

    Blanking out candler

--- Application/Inc/CANdler.h
+++ Application/Inc/CANdler.h
@@ -3,6 +3,8 @@
 #ifndef CANDLER_H
 #define CANDLER_H
 
+extern int numberOfBadMessages;
+
 void handleCANMessage(uint16_t msgID, uint8_t srcID, uint8_t *data, uint32_t length, uint32_t timestamp);
 
 typedef struct {
---
+++ Application/Src/CANdler.c
@@ -0,0 +1,49 @@
+#include <stdint.h>
+
+#include "CANdler.h"
+#include "msgIDs.h"
+
+void handleCANMessage(uint16_t msgID, uint8_t srcID, uint8_t *data, uint32_t length, uint32_t timestamp) {
+    UNUSED(timestamp);
+
+    switch(msgID) {
+        case MSG_DEBUG_FD:
+            if (length > 64) {
+                numberOfBadMessages++;
+                return;
+            } else {
+                numberOfBadMessages += (numberOfBadMessages > 0) ? -1 : 0;
+            }
+
+            char* debug2String = (char*)data;
+            UNUSED(debug2String);
+
+            break;
+        case MSG_STEERING_CONFIG:
+            // RESERVED
+            break;
+        case MSG_TCM_STATUS:
+            if (length != 8) {
+                numberOfBadMessages++;
+                return;
+            } else {
+                numberOfBadMessages += (numberOfBadMessages > 0) ? -1 : 0;
+            }
+
+            TCMStatusMsg* tcmStatusMsg = (TCMStatusMsg*)data;
+            UNUSED(tcmStatusMsg);
+
+            break;
+        case MSG_TCM_RESOURCE_UTILIZATION:
+            if (length != 7) {
+                numberOfBadMessages++;
+                return;
+            } else {
+                numberOfBadMessages += (numberOfBadMessages > 0) ? -1 : 0;
+            }
+
+            TCMResourceUtilizationMsg* tcmResUtilMsg = (TCMResourceUtilizationMsg*)data;
+            UNUSED(tcmResUtilMsg);
+            break;
+    }
+}
