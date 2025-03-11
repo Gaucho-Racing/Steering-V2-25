149ad3fdd34ae809d004aa1e5c1733c249193ce3
Daniel Hansen <dchansen06@gmail.com>
2025-03-10 23:18:11 -0700

    Candler header file

---
+++ Application/Inc/CANdler.h
@@ -0,0 +1,35 @@
+#include <stdint.h>
+
+#ifndef CANDLER_H
+#define CANDLER_H
+
+void handleCANMessage(uint16_t msgID, uint8_t srcID, uint8_t *data, uint32_t length, uint32_t timestamp);
+
+typedef struct {
+    uint8_t CMEandTME;
+    uint8_t RME;
+    uint8_t buttonMap;
+} SteeringStatusMsg;
+
+// EMTPY FOR NOW
+typedef struct {
+} SteeringConfigMsg;
+
+typedef struct {
+    uint8_t signalBits;
+    uint16_t ping;
+    uint32_t cacheSize;
+    uint8_t reserved;
+} TCMStatusMsg;
+
+typedef struct {
+    uint8_t cpuUsage;
+    uint8_t gpuUsage;
+    uint8_t memoryUsage;
+    uint8_t storageUsage;
+    uint8_t powerConsumption;
+    uint8_t cpuTemp;
+    uint8_t gpuTemp;
+} TCMResourceUtilizationMsg;
+
+#endif
