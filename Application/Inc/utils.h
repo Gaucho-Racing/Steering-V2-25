3460ed35398264f203fb2e46d268cd0285be7b16
Daniel Hansen <dchansen06@gmail.com>
2025-03-10 23:19:54 -0700

    Added utils functions

---
+++ Application/Inc/utils.h
@@ -0,0 +1,9 @@
+#include <stdint.h>
+
+#ifndef UTILS_H
+#define UTILS_H
+
+uint8_t getBit(uint8_t number, uint8_t indexFromLeft);
+uint8_t getBits(uint8_t number, uint8_t indexFromLeft, uint8_t length);
+
+#endif
