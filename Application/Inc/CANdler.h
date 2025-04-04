13bf711f9e6fe4447c0261c93b97e0adf05d1afa
ahoysal <aagrimhoysal@gmail.com>
2025-04-03 21:02:36 -0700

    incomping message
    Co-authored-by: ManasijaBhargavan <ManasijaBhargavan@users.noreply.github.com>
    Co-authored-by: Daniel Hansen <dchansen06@users.noreply.github.com>

--- Application/Inc/CANdler.h
+++ Application/Inc/CANdler.h
@@ -1,8 +1,8 @@
-#ifndef CANDLER_H
-#define CANDLER_H
-
 #include <stdint.h>
 #include <stdbool.h>
+
+#ifndef CANDLER_H
+#define CANDLER_H
 
 #define LOCAL_GR_ID GR_STEERING_WHEEL
 
@@ -20,22 +20,4 @@ typedef struct {
 typedef struct {
     bool DONOTUSE;
 } SteeringConfigMsg;
-
-typedef struct {
-    uint8_t signalBits;
-    uint16_t ping;
-    uint32_t cacheSize;
-    uint8_t reserved;
-} TCMStatusMsg;
-
-typedef struct {
-    uint8_t cpuUsage;
-    uint8_t gpuUsage;
-    uint8_t memoryUsage;
-    uint8_t storageUsage;
-    uint8_t powerConsumption;
-    uint8_t cpuTemp;
-    uint8_t gpuTemp;
-} TCMResourceUtilizationMsg;
-
 #endif
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -41,12 +41,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "lvgl/lvgl.h"
-#include "lvgl/demos/lv_demos.h"
-#include "lvgl_port_display.h"
-#include "msgIDs.h"
-#include "CANdler.h"
-#include "grIDs.h"
+#include "steering.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -81,22 +76,6 @@ void MX_FREERTOS_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-SteeringStatusMsg constructStatus() {
-  SteeringStatusMsg data;
-  data.buttonMap = 0 |
-    (HAL_GPIO_ReadPin(BUTTON_1_GPIO_Port, BUTTON_1_Pin) << 7) |
-    (HAL_GPIO_ReadPin(BUTTON_2_GPIO_Port, BUTTON_2_Pin) << 6) |
-    (HAL_GPIO_ReadPin(BUTTON_3_GPIO_Port, BUTTON_3_Pin) << 5) | 
-    (HAL_GPIO_ReadPin(BUTTON_4_GPIO_Port, BUTTON_4_Pin) << 4);
-  // TODO: update encoder values for current, torque map, and regen encoders
-
-  return data;
-}
-
-void sendStatus() {
-  SteeringStatusMsg status = constructStatus();
-  writeToECU(MSG_STEERING_STATUS, (uint8_t*)(&status), 4);
-}
 /* USER CODE END 0 */
 
 /**
@@ -153,152 +132,7 @@ int main(void)
   MX_TIM15_Init();
   MX_FLASH_Init();
   /* USER CODE BEGIN 2 */
-
-
-  if (HAL_TIM_PWM_Start(&htim15, TIM_CHANNEL_1) != HAL_OK) {
-    /* PWM Generation Error */
-    Error_Handler();
-  }
-
-  /* reset display */
-  HAL_GPIO_WritePin(LCD_DISP_RESET_GPIO_Port, LCD_DISP_RESET_Pin, GPIO_PIN_SET);
-
-  /* initialize LVGL framework */
-  lv_init();
-
-  /* initialize display and touchscreen */
-  lvgl_display_init();
-  
-  /* Change Active Screen's background color */
-  lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0xff0000), LV_PART_MAIN);
-  lv_obj_set_style_text_color(lv_screen_active(), lv_color_hex(0xffffff), LV_PART_MAIN);
-
-  // /* Create a spinner */
-  // lv_obj_t * spinner = lv_spinner_create(lv_screen_active()/*, 1000, 60*/);
-  // lv_obj_set_size(spinner, 64, 64);
-  
-
-
-  /*
-  static lv_style_t style;
-  lv_style_init(&style);
-  lv_style_set_radius(&style, 5);
-
-  lv_style_set_width(&style, 150);
-  lv_style_set_height(&style, LV_SIZE_CONTENT);
-
-  lv_style_set_pad_ver(&style, 20);
-  lv_style_set_pad_left(&style, 5);
-
-  lv_style_set_x(&style, lv_pct(50));
-  lv_style_set_y(&style, 80);
-
-  lv_obj_t * obj = lv_obj_create(lv_scr_act());
-  lv_obj_add_style(obj, &style, 0);
-
-  lv_obj_t * label = lv_label_create(obj);
-  lv_label_set_text(label, "Hello");
-
-  */
-
-  // lv_obj_t * flexCol1 = lv_obj_create(scr);
-  // lv_obj_set_layout(flexCol1, LV_LAYOUT_FLEX);
-  // lv_obj_set_flex_flow(flexCol1, LV_FLEX_FLOW_COLUMN);
-
-  // lv_obj_t * flexRow1 = lv_obj_create(flexCol1);
-  // lv_obj_t * flexRow2 = lv_obj_create(flexCol1);
-  // lv_obj_set_layout(flexRow1, LV_LAYOUT_FLEX);
-  // lv_obj_set_flex_flow(flexRow1, LV_FLEX_FLOW_ROW);
-  // lv_obj_set_layout(flexRow2, LV_LAYOUT_FLEX);
-  // lv_obj_set_flex_flow(flexRow2, LV_FLEX_FLOW_ROW);
-
-  // // Dummy screen test
-
-  // lv_obj_t * label1 = lv_obj_create(flexRow1);
-  // lv_obj_t * label2 = lv_obj_create(flexRow2);
-
-  // lv_label_set_text(label1, "Skibidi toilet rizz.");
-  // lv_label_set_text(label2, "Courtesy of Gaucho Racing");
-
-  // Real code 
-
-  /*
-   * https://docs.lvgl.io/master/details/widgets/label.html - for changing label texts
-   * https://forum.lvgl.io/t/backgroud-colour/2036 - for changing color
-   * 
-   * 
-   * 
-   * 
-  */
-
-  // Top Row
-
-  // lv_obj_t * flexRow1Col1 = lv_obj_create(flexRow1);
-  // lv_obj_set_layout(flexRow1Col1, LV_LAYOUT_FLEX);
-  // lv_obj_set_flex_flow(flexRow1Col1, LV_FLEX_FLOW_COLUMN);
-  // lv_obj_t * flexRow1Col2 = lv_obj_create(flexRow1);
-  // lv_obj_set_layout(flexRow1Col2, LV_LAYOUT_FLEX);
-  // lv_obj_set_flex_flow(flexRow1Col2, LV_FLEX_FLOW_COLUMN);
-  // lv_obj_t * flexRow1Col3 = lv_obj_create(flexRow1);
-  // lv_obj_set_layout(flexRow1Col3, LV_LAYOUT_FLEX);
-  // lv_obj_set_flex_flow(flexRow1Col3, LV_FLEX_FLOW_COLUMN);
-
-  // // Bottom Row
-
-  // lv_obj_t * flexRow2Col1 = lv_obj_create(flexRow2);
-  // lv_obj_set_layout(flexRow2Col1, LV_LAYOUT_FLEX);
-  // lv_obj_set_flex_flow(flexRow2Col1, LV_FLEX_FLOW_COLUMN);
-  // lv_obj_t * flexRow2Row1 = lv_obj_create(flexRow2);
-  // lv_obj_set_layout(flexRow2Row1, LV_LAYOUT_FLEX);
-  // lv_obj_set_flex_flow(flexRow2Row1, LV_FLEX_FLOW_ROW);
-
-  // // Power - voltage, SOC, and total power
-
-  // lv_obj_t * voltageWidget = lv_obj_create(flexRow1Col1);
-  // lv_obj_t * socWidget = lv_obj_create(flexRow1Col1);
-  // lv_obj_t * totalPowerWidget = lv_obj_create(flexRow1Col1);
-
-  // // Main - speed, state, and warning
-
-  // lv_obj_t * speedWidget = lv_obj_create(flexRow1Col2);
-  // lv_obj_t * stateWidget = lv_obj_create(flexRow1Col2);
-  // lv_obj_t * warningWidget = lv_obj_create(flexRow1Col2);
-
-  // // Dials 
-
-  // lv_obj_t * regenEncoder = lv_obj_create(flexRow1Col3);
-  // lv_obj_t * currentEncoder = lv_obj_create(flexRow1Col3);
-  // lv_obj_t * torqueMapEncoder = lv_obj_create(flexRow1Col3);
-
-
-  // // Car diagram
-
-  // lv_obj_t * flexRow2Col1Row1 = lv_obj_create(flexRow2Col1);
-  // lv_obj_set_layout(flexRow2Col1Row1, LV_LAYOUT_FLEX);
-  // lv_obj_set_flex_flow(flexRow2Col1Row1, LV_FLEX_FLOW_ROW);
-  // lv_obj_t * flexRow2Col1Row2 = lv_obj_create(flexRow2Col1);
-  // lv_obj_set_layout(flexRow2Col1Row2, LV_LAYOUT_FLEX);
-  // lv_obj_set_flex_flow(flexRow2Col1Row2, LV_FLEX_FLOW_ROW);
-  // lv_obj_t * flexRow2Col1Row3 = lv_obj_create(flexRow2Col1);
-  // lv_obj_set_layout(flexRow2Col1Row3, LV_LAYOUT_FLEX);
-  // lv_obj_set_flex_flow(flexRow2Col1Row3, LV_FLEX_FLOW_ROW);
-
-  // lv_obj_t * wheelFLWidget = lv_obj_create(flexRow2Col1Row1);
-  // lv_obj_t * wheelFRWidget = lv_obj_create(flexRow2Col1Row1);
-  // lv_obj_t * wheelRLWidget = lv_obj_create(flexRow2Col1Row3);
-  // lv_obj_t * wheelRRWidget = lv_obj_create(flexRow2Col1Row3);
-
-  // lv_obj_t * carDirectionWidget = lv_obj_create(flexRow2Col1Row2);
-
-  // // Temperature, battery, inverter, motor, water coolant, tire, brake
-
-  // lv_obj_t * batteryTempWidget = lv_obj_create(flexRow2Row1);
-  // lv_obj_t * inverterTempWidget = lv_obj_create(flexRow2Row1);
-  // lv_obj_t * motorTempWidget = lv_obj_create(flexRow2Row1);
-  // lv_obj_t * waterCoolantTempWidget = lv_obj_create(flexRow2Row1);
-  // lv_obj_t * tireTempWidget = lv_obj_create(flexRow2Row1);
-  // lv_obj_t * brakeTempWidget = lv_obj_create(flexRow2Row1);
-
+  initLVGL();
   /* USER CODE END 2 */
 
   /* Init scheduler */
