6918c8dc28a58e24b850f194be26c305a9bcf178
Daniel Hansen <dchansen06@gmail.com>
2025-04-07 20:48:40 -0700

    Builds everything nicely I hope

--- Application/Inc/utils.h
+++ Application/Inc/utils.h
@@ -1,9 +1,16 @@
 #include <stdint.h>
+#include <stdio.h>
 
 #ifndef UTILS_H
 #define UTILS_H
 
 #define TICK_FREQ 1000
+
+#ifdef LOGOMATIC_ENABLED
+    #define LOGOMATIC(...) printf(__VA_ARGS__)
+#else
+    #define LOGOMATIC(...) UNUSED(NULL)
+#endif
 
 uint32_t millis(void);
 uint8_t getBit(uint8_t number, uint8_t indexFromLeft);
