ccb0715f4c3b095d97c40d625442242ec833b71d
Daniel Hansen <dchansen06@gmail.com>
2025-04-10 19:50:57 -0700

    More work in encoders
    Co-authored-by: Khilan Surapaneni <KhilanSurapaneni@users.noreply.github.com>
    Co-authored-by: Aagrim Hoysal <ahoysal@users.noreply.github.com>
    Co-authored-by: ManasijaBhargavan <ManasijaBhargavan@users.noreply.github.com>

--- Application/Inc/encoders.h
+++ Application/Inc/encoders.h
@@ -4,10 +4,14 @@
     #define ENCODERS_H
 
     typedef enum {
-        ENC_POWER,
+        ENC_CURRENT,
         ENC_TORQUE,
         ENC_REGEN,
     } Encoder;
+
+    typedef struct {
+        uint
+    } encoderPercentages;
 
     uint16_t getStatus(Encoder encoder);
     void pollEncoderDials(void);
--- Application/Src/encoders.c
+++ Application/Src/encoders.c
@@ -5,32 +5,35 @@
 #include "stm32u5xx_hal.h"
 
 typedef struct {
-    uint8_t power;
+    uint8_t current;
     uint8_t regen;
     uint8_t torque;
 } EncoderPositions;
 
+EncoderPositions percentage = {0};
+EncoderPositions startingAbsolutePosition = {0};
+
+uint8_t getAbsolutePosition(Encoder encoder)
+{
+    uint8_t result = 0;
+    switch(encoder) {
+        case ENC_CURRENT:
+            result = HAL_GPIO_ReadPin(TME_P1_GPIO_Port, TME_P1_Pin) | (HAL_GPIO_ReadPin(TME_P2_GPIO_Port, TME_P2_Pin) << 1)
+            result += HAL_GPIO_ReadPin(TME_P3_GPIO_Port, TME_P3_Pin) << 2;
+            result += HAL_GPIO_ReadPin(TME_P4_GPIO_Port, TME_P4_Pin) << 3;
+            break;
+
+        case ENC_REGEN:
+
+            break;
+
+        case ENC_TORQUE:
+
+            break;
+    }
+}
+
 uint16_t getStatus(Encoder encoder)
 {
-
-}
-
-void pollEncoderDials(void)
-{
-    if (HAL_GPIO_ReadPin(BUTTON_1_GPIO_Port, BUTTON_1_Pin) == GPIO_PIN_SET) {
-        
-    }
-}
-
-uint16_t pollSpecificEncoderDial(Encoder encoder)
-{
-    switch(encoder) {
-        case ENC_POWER:
-            break;
-        case ENC_REGEN:
-            break;
-        case ENC_TORQUE:
-            break;
-    }
-    return 
+    
 }
