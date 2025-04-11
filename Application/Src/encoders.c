37b3a3be20f0837959439aa1cbc01110403e211b
Daniel Hansen <dchansen06@gmail.com>
2025-04-10 19:27:43 -0700

    More work in steering and encoders

--- Application/Src/encoders.c
+++ Application/Src/encoders.c
@@ -1,4 +1,14 @@
+#include <stdint.h>
+
 #include "encoders.h"
+#include "main.h"
+#include "stm32u5xx_hal.h"
+
+typedef struct {
+    uint8_t power;
+    uint8_t regen;
+    uint8_t torque;
+} EncoderPositions;
 
 uint16_t getStatus(Encoder encoder)
 {
@@ -7,5 +17,20 @@ uint16_t getStatus(Encoder encoder)
 
 void pollEncoderDials(void)
 {
-    
+    if (HAL_GPIO_ReadPin(BUTTON_1_GPIO_Port, BUTTON_1_Pin) == GPIO_PIN_SET) {
+        
+    }
+}
+
+uint16_t pollSpecificEncoderDial(Encoder encoder)
+{
+    switch(encoder) {
+        case ENC_POWER:
+            break;
+        case ENC_REGEN:
+            break;
+        case ENC_TORQUE:
+            break;
+    }
+    return 
 }
