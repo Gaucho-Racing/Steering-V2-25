4cc2496f005a59f0eb0cbfd6e03d21b42ee3ede5
Daniel Hansen <dchansen06@gmail.com>
2025-04-17 21:18:29 -0700

    Added color hex keys

--- Application/Inc/gui.h
+++ Application/Inc/gui.h
@@ -3,11 +3,16 @@
 #include "lvgl/lvgl.h"
 
 #ifndef STEERING_GUI_H
-#define STEERING_GUI_H
+    #define STEERING_GUI_H
 
-#define GRID_COLUMNS 9
-#define GRID_ROWS 9
+    #define GRID_COLUMNS 9
+    #define GRID_ROWS 9
 
-void initGrid(lv_obj_t* screen, lv_obj_t* cells[]);
+    // See Brand Guidelines
+    #define GR_COLOR_PINK 0xEF0DA1
+    #define GR_COLOR_PURPLE 0x7920FF
+    #define GR_COLOR_NAVY 0x195297
+    #define GR_COLOR_GRAY 0x9AA3B0
 
+    void initGrid(lv_obj_t* screen, lv_obj_t* cells[]);
 #endif
