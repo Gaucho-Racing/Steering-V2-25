da992e3a429a7b0151f6fc35040681f553daac8e
Daniel Hansen <dchansen06@gmail.com>
2025-05-17 01:17:46 -0700

    Oopsies small compile-time warning fixed

--- Application/Src/CANdler.c
+++ Application/Src/CANdler.c
@@ -111,7 +111,7 @@ void handleCANMessage(uint16_t msgID, uint8_t srcID, uint8_t *data, uint32_t len
                 numberOfBadMessages += (numberOfBadMessages > 0) ? -1 : 0;
             }
 
-            memcpy(incomingData.ecuPingMap, data, length);
+            memcpy((uint8_t*)incomingData.ecuPingMap, data, length);
 
             break;
 
