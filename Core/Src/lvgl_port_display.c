e6e131afc470cd61f8f6d38a148cb3d67f614815
ahoysal <aagrimhoysal@gmail.com>
2025-05-19 20:49:53 -0700

    removed redundant DMA

--- Core/Src/lvgl_port_display.c
+++ Core/Src/lvgl_port_display.c
@@ -20,7 +20,6 @@ static void disp_flush_complete(DMA2D_HandleTypeDef *);
  *  STATIC VARIABLES
  **********************/
 
-volatile bool flushed = true;
 lv_display_t *display;
 
 
@@ -45,7 +44,6 @@ void lvgl_display_init(void)
   // lv_disp_drv_init(&disp_drv);
   display = lv_display_create(MY_DISP_HOR_RES, MY_DISP_VER_RES);
   lv_display_set_flush_cb(display, disp_flush);
-  //lv_display_set_flush_wait_cb(display, disp_flush_wait);
   lv_display_set_buffers(display, (void*) &buf_1, NULL, sizeof(buf_1), LV_DISPLAY_RENDER_MODE_PARTIAL);
 
   HAL_LTDC_SetAddress(&hltdc, (uint32_t) &buf_1, LTDC_LAYER_1);
@@ -58,9 +56,6 @@ void lvgl_display_init(void)
   // disp_drv.flush_cb = disp_flush;
   // disp_drv.full_refresh = 0;
   // disp_drv.direct_mode = 1;
-
-  /* interrupt callback for DMA2D transfer */
-  HAL_DMA2D_RegisterCallback(&hdma2d, HAL_DMA2D_TRANSFERCOMPLETE_CB_ID, disp_flush_complete);
 
   /* set a display buffer */
   // disp_drv.draw_buf = &disp_buf;
@@ -75,30 +70,6 @@ void lvgl_display_init(void)
 
 static void disp_flush(lv_display_t *drv, const lv_area_t *area, uint8_t *color_p)
 {
-  // TODO: update such that drv --> lv_display_t
-  // TODO: update such that color_p --> uint8_t
   UNUSED(drv);
-  flushed = false;
-
-  lv_coord_t width = lv_area_get_width(area);
-  lv_coord_t height = lv_area_get_height(area);
-
-  DMA2D->CR = 0x0U << DMA2D_CR_MODE_Pos;
-  DMA2D->FGPFCCR = DMA2D_INPUT_RGB565;
-  DMA2D->FGMAR = (uint32_t)color_p;
-  DMA2D->FGOR = 0;
-  DMA2D->OPFCCR = DMA2D_OUTPUT_RGB565;
-  DMA2D->OMAR = hltdc.LayerCfg[0].FBStartAdress + 2 * (area->y1 * MY_DISP_HOR_RES + area->x1);
-  DMA2D->OOR = MY_DISP_HOR_RES - width;
-  DMA2D->NLR = (width << DMA2D_NLR_PL_Pos) | (height << DMA2D_NLR_NL_Pos);
-  DMA2D->IFCR = 0x3FU;
-  DMA2D->CR |= DMA2D_CR_TCIE;
-  DMA2D->CR |= DMA2D_CR_START;
-}
-
-static void disp_flush_complete(DMA2D_HandleTypeDef *hdma2d)
-{
-  UNUSED(hdma2d);
-
-  lv_display_flush_ready(display);
+  lv_display_flush_ready(display); // LTDC handles flushing screen
 }
